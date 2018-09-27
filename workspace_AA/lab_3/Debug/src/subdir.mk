999aec3dbe5a64c5fd1ad6dbe6695cc23a8526cc
alialnaji97 <43251163+alialnaji97@users.noreply.github.com>
2018-09-27 14:29:30 -0700

    fucking assembly

--- workspace_AA/lab_3/Debug/src/subdir.mk
+++ workspace_AA/lab_3/Debug/src/subdir.mk
@@ -3,12 +3,16 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../src/GetFib.s 
+
 C_SRCS += \
 ../src/cr_startup_lpc11xx.c \
 ../src/crp.c \
 ../src/main.c 
 
 OBJS += \
+./src/GetFib.o \
 ./src/cr_startup_lpc11xx.o \
 ./src/crp.o \
 ./src/main.o 
@@ -20,6 +24,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU Assembler'
+	arm-none-eabi-gcc -c -x assembler-with-cpp -DDEBUG -D__CODE_RED -DCORE_M0 -D__USE_CMSIS=CMSIS_CORE_LPC11xx -D__LPC11XX__ -D__REDLIB__ -I"C:\Users\dell\Documents\GitHub\DDL\workspace_AA\lab_3\inc" -I"C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc" -g3 -mcpu=cortex-m0 -mthumb -specs=redlib.specs -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
