c5866d03f11e9698164be6b7b7eb04fe25305939
alialnaji97 <43251163+alialnaji97@users.noreply.github.com>
2018-10-02 13:18:24 -0700

    new proj

---
+++ workspace_AA/lab4/Debug/driver/crp.d
@@ -0,0 +1,22 @@
+driver/crp.o driver/crp.d: ../driver/crp.c \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\lab4\config/driver_config.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/LPC11xx.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cm0.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmInstr.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmFunc.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/system_LPC11xx.h \
+ ../driver/crp.h
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\lab4\config/driver_config.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/LPC11xx.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cm0.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmInstr.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmFunc.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/system_LPC11xx.h:
+
+../driver/crp.h:
---
+++ workspace_AA/lab4/Debug/driver/gpio.d
@@ -0,0 +1,22 @@
+driver/gpio.o driver/gpio.d: ../driver/gpio.c \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\lab4\config/driver_config.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/LPC11xx.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cm0.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmInstr.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmFunc.h \
+ C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/system_LPC11xx.h \
+ ../driver/gpio.h
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\lab4\config/driver_config.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/LPC11xx.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cm0.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmInstr.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/core_cmFunc.h:
+
+C:\Users\dell\Documents\GitHub\DDL\workspace_AA\CMSIS_CORE_LPC11xx\inc/system_LPC11xx.h:
+
+../driver/gpio.h:
