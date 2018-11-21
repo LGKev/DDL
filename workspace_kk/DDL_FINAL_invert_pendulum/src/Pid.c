a80398e1322b5e407ea6c1473604286219eafb2d
kevin kuwata <keku4547@colorado.edu>
2018-11-21 11:37:22 -0700

    updated the pid loop code with a link

--- workspace_kk/DDL_FINAL_invert_pendulum/src/Pid.c
+++ workspace_kk/DDL_FINAL_invert_pendulum/src/Pid.c
@@ -2,31 +2,35 @@
  * Pid.c
  *
  *  Created on: Nov 21, 2018
- *      Author: kkuwata
+ *      Author: http://brettbeauregard.com/blog/2011/04/improving-the-beginners-pid-introduction/
  */
 #include "Pid.h"
-
 /*working variables*/
 unsigned long lastTime;
 double Input, Output, Setpoint;
 double errSum, lastErr;
 double kp, ki, kd;
-void Compute(void){
+void Compute()
+{
+   /*How long since we last calculated*/
+   unsigned long now = millis();
+   double timeChange = (double)(now - lastTime);
 
    /*Compute all the working error variables*/
    double error = Setpoint - Input;
- //  errSum += (error * timeChange);
-   //double dErr = (error - lastErr) / timeChange;
+   errSum += (error * timeChange);
+   double dErr = (error - lastErr) / timeChange;
 
    /*Compute PID Output*/
-//   Output = kp * error + ki * errSum + kd * dErr;
+   Output = kp * error + ki * errSum + kd * dErr;
 
    /*Remember some variables for next time*/
-  // lastErr = error;
-   //lastTime = now;
+   lastErr = error;
+   lastTime = now;
 }
 
-void SetTunings(double Kp, double Ki, double Kd){
+void SetTunings(double Kp, double Ki, double Kd)
+{
    kp = Kp;
    ki = Ki;
    kd = Kd;
